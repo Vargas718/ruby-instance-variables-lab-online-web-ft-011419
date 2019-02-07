3cc8026870d3d2bbdc61e8370807ac2499497419
Rafael Vargas <vargas718@gmail.com>
2019-02-07 04:07:13 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
  
  
@@ -12,4 +12,6 @@ class Dog
 end
 lassie = Dog.new
 
-lassie.name
+lassie.name = "Lassie"
+
+puts lassie.name
